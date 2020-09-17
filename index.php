9227f60634a611fcdf7bcdcb62277dc6fb09cd0e
Pietro Sette <pietrosette@gmail.com>
2020-09-17 17:21:20 +0200

    header done

---
+++ index.php
@@ -0,0 +1,37 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Domande frequenti – Privacy e termini – Google</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <div class="logo-bar">
+                <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt="Google Logo">
+                <a href="#">Privacy e Termini</a>
+            </div>
+            <div class="navbar">
+                <div class="links">
+                    <a href="#">Introduzione</a>
+                    <a href="#">Norme sulla privacy</a>
+                    <a href="#">Termini di servizio</a>
+                    <a href="#">Tecnologie</a>
+                    <a href="#">Domande Frequenti</a>
+                </div>
+                <div class="google-account">
+                    <a href="#">Account Google</a>
+                </div>
+            </div>
+        </header>
+        <main>
+
+        </main>
+        <footer></footer>
+    </body>
+</html>
